8bfeb2ab414278d84d0d889dd39e0c990d795400
AyeletNissan <ayelet.nissan@posr.runi.ac.il>
2023-12-20 19:08:59 +0200

    Update GenThree.java

--- GenThree.java
+++ GenThree.java
@@ -1,4 +1,4 @@
-public class Gen3 {
+public class GenThree {
     public static void main(String[] args){
         int low = Integer.parseInt(args[0]);
         int high = Integer.parseInt(args[1]);
