2ce7919d1f82fb72fb221223239bc5e814b25b78
AyeletNissan <154340898+AyeletNissan@users.noreply.github.com>
2023-12-20 18:21:41 +0200

    adding triangle

--- GenThree.java
+++ GenThree.java
@@ -1,9 +1,16 @@
-/*
- * Generates three random integers, each in a given range [a,b),
- * prints them, and then prints the minimal number that was generated.
- */
-public class GenThree {
-	public static void main(String[] args) {
-		// Put your code here	
-	}
+public class Gen3 {
+    public static void main(String[] args){
+        int low = Integer.parseInt(args[0]);
+        int high = Integer.parseInt(args[1]);
+
+        int a = (int)((Math.random()*(high-low))+low);
+        int b = (int)((Math.random()*(high-low))+low);
+        int c = (int)((Math.random()*(high-low))+low);
+        int min = (int)Math.min(c, Math.min(a, b));
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println("The minimal generated number was " + min);
+    }
 }
